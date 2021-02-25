c20873b077f867bd59cffc6e9d3c2188d4628aca
shoffkarim <k4r1mch1k@gmail.com>
2021-02-25 10:31:41 +0500

    add static cart

--- src/components/SortPopup.jsx
+++ src/components/SortPopup.jsx
@@ -15,7 +15,8 @@ const SortPopup = React.memo(function SortPopup({items, activeSortType, onClickS
   };
 
   const handleClickOutside = (e) => {
-    if(!e.path.includes(sortRef.current)){
+    const path = e.path || (e.composedPath && e.composedPath());
+    if(!path.includes(sortRef.current)){
       setVisiblePopup(false);
     }
   };
